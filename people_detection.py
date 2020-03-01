fd1f23d5260a8e7fa4994cb1ad4486ced751c960
Faisal <faisal.hoque@hotmail.com>
2020-03-01 17:40:36 +0000

    Integrate object detection

--- people_detection.py
+++ people_detection.py
@@ -1,9 +1,40 @@
 import cv2
+import numpy as np
 
 cap = cv2.VideoCapture(0)
 
+# Classes the model is trained to predict
+CLASSES = ["background", "aeroplane", "bicycle", "bird", "boat",
+           "bottle", "bus", "car", "cat", "chair", "cow", "diningtable",
+           "dog", "horse", "motorbike", "person", "pottedplant", "sheep",
+           "sofa", "train", "tvmonitor"]
+# Random colour for each class
+COLORS = np.random.uniform(0, 255, size=(len(CLASSES), 3))
+
+# Load model
+net = cv2.dnn.readNetFromCaffe("model/MobileNetSSD_deploy.prototxt", "model/MobileNetSSD_deploy.caffemodel")
+
 while True:
     ret, image = cap.read()
+
+    (h, w) = image.shape[:2]
+    blob = cv2.dnn.blobFromImage(cv2.resize(image, (300, 300)), 0.007843, (300, 300), 127.5)
+
+    net.setInput(blob)
+    detections = net.forward()
+
+    for i in np.arange(0, detections.shape[2]):
+        confidence = detections[0, 0, i, 2]
+
+        if confidence > 0.5:
+            idx = int(detections[0, 0, i, 1])
+            box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
+            (startX, startY, endX, endY) = box.astype('int')
+
+            label = '{}: {:.2f}%'.format(CLASSES[idx], confidence * 100)
+            cv2.rectangle(image, (startX, startY), (endX, endY), COLORS[idx], 2)
+            y = startY - 15 if startY - 15 > 15 else startY + 15
+            cv2.putText(image, label, (startX, y), cv2.FONT_HERSHEY_SIMPLEX, 0.5, COLORS[idx], 2)
 
     cv2.imshow('People Detection', image)
 
